299213a892daf0528fa4603f967622a1b0f5266c
nithinsubbiah <nithinsubbiah@gmail.com>
2025-11-21 03:09:29 +0000

    [Wave] Modify Wave attention backend
    Signed-off-by: nithinsubbiah <nithinsubbiah@gmail.com>

--- python/sglang/srt/layers/attention/wave_ops/decode_attention.py
+++ python/sglang/srt/layers/attention/wave_ops/decode_attention.py
@@ -100,21 +100,6 @@ def decode_attention_intermediate_arrays_shapes(
     return get_paged_decode_intermediate_arrays_shapes(shape, max_kv_splits)
 
 
-def decode_attention_intermediate_arrays_shapes(
-    num_seqs, head_size_kv, num_query_heads, max_kv_splits
-):
-    # Not all fields are used, but we need to pass them to the function
-    shape = paged_decode_attention_shape(
-        num_query_heads=num_query_heads,
-        num_kv_heads=0,
-        head_size=0,
-        head_size_kv=head_size_kv,
-        block_size=0,
-        num_seqs=num_seqs,
-    )
-    return get_paged_decode_intermediate_arrays_shapes(shape, max_kv_splits)
-
-
 def decode_attention_wave(
     q,
     k_buffer,
